9e30ceb9a7df862a832305a65dfa45fba971f91b
mattcuento-RL <matt.cuento@ridgelineapps.com>
2021-06-24 10:01:05 -0700

    Added eslint and pre-commit hook for linting

--- handler.js
+++ handler.js
@@ -1,14 +1,12 @@
-export const hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v2.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
-  };
-};
+const message = ({ time, ...rest }) => new Promise((resolve) => setTimeout(() => {
+  resolve(`${rest.copy} (with a delay)`);
+}, time * 1000));
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
+const hello = async () => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    message: `Go Serverless v2.0! ${(await message({ time: 1, copy: 'Your function executed successfully!' }))}`,
+  }),
+});
+
+export default hello;
