f9c71f605204df53313839bfee12362544b65997
mattcuento-RL <matt.cuento@ridgelineapps.com>
2021-06-24 13:12:47 -0700

    passing test

---
+++ tests/handler.test.js
@@ -0,0 +1,12 @@
+import hello from '../handler';
+
+test('hello', async () => {
+  const event = 'event';
+  const context = 'context';
+  const callback = (error, response) => {
+    expect(response.statusCode).toEqual(200);
+    expect(typeof response.body).toBe("string");
+  };
+
+  await hello(event, context, callback);
+});
