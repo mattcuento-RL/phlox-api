fdee937c38cf7af40a1b72ecba21aec7a0dbab69
mattcuento-RL <matt.cuento@ridgelineapps.com>
2021-06-24 16:24:26 -0700

    created listings crud ops, still failing

---
+++ create.js
@@ -0,0 +1,27 @@
+import * as uuid from 'uuid';
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
+
+const main = handler(async (event, _context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.listingTable,
+    Item: {
+      // The attributes of the item to be created
+      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+      listingId: uuid.v1(), // A unique uuid
+      title: data.title, // Parsed from request body
+      description: data.description, // Parsed from request body
+      category: data.category,
+      rules: data.rules,
+      imageUrls: data.imageUrls,
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return params.Item;
+});
+
+export default main;
---
+++ list.js
@@ -0,0 +1,24 @@
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
+
+const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.listingTable,
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    // KeyConditionExpression: "userId = :userId",
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be the id of the author
+    // ExpressionAttributeValues: {
+    //  ":userId": event.requestContext.identity.cognitoIdentityId, // The id of the author,
+    // },
+  };
+
+  const result = await dynamoDb.query(params);
+
+  // Return the matching list of items in response body
+  return result.Items;
+});
+
+export default main;
