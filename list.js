3c6dbb209bd56d34a8147f87518eabf25575d877
mattcuento-RL <matt.cuento@ridgelineapps.com>
2021-06-28 09:14:58 -0700

    created scan for lender requests

--- list.js
+++ list.js
@@ -16,3 +16,22 @@ export const main = handler(async (event, context) => {
   } while (typeof items.LastEvaluatedKey !== 'undefined');
   return scanResults;
 });
+
+export const getLenderRequests = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.requestTable,
+    // 'FilterExpression' defines the condition for the query
+    // - 'listingAuthorId = :listingAuthorId': only return items with matching 'listingAuthorId'
+    FilterExpression: 'listingAuthorId = :listingAuthorId',
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':listingAuthorId': defines 'listingAuthorId' to be the id of the author
+    ExpressionAttributeValues: {
+      ':listingAuthorId': event.requestContext.identity.cognitoIdentityId, // The id of the author,
+    },
+  };
+
+  const result = await dynamoDb.scan(params);
+
+  // Return the matching list of items in response body
+  return result.Items;
+});
