1f74145268758c7037f5620bd04b3c724b0e29e6
Urvashi Mahto <urvashi.mahto@Urvashis-MacBook-Pro.local>
2021-06-29 12:02:42 -0700

    updated delete function to update matching requests as canceled and archived

--- delete.js
+++ delete.js
@@ -13,5 +13,38 @@ export const main = handler(async (event, context) => {
 
   await dynamoDb.delete(params);
 
+  const params2 = {
+    TableName: process.env.requestTable,
+    // 'FilterExpression' defines the condition for the query
+    // - 'listingId = :listingId': only return items with matching 'listingId'
+    FilterExpression: 'listingId = :listingId',
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':listingId': defines 'listingAuthorId' to be the id of the author
+    ExpressionAttributeValues: {
+      ':listingId': event.pathParameters.id, // The id of the author,
+    },
+  };
+
+  const result = await dynamoDb.scan(params2);
+
+  if (result.Items.length > 0) {
+    result.Items.forEach((request) => {
+      const params3 = {
+        TableName: process.env.requestTable,
+        Key: {
+          userId: request.userId, // The id of the author
+          requestId: request.requestId, // The id of the note from the path
+        },
+        UpdateExpression: 'SET requestStatus = :requestStatus, archived = :archived',
+        ExpressionAttributeValues: {
+          ':requestStatus': 3,
+          ':archived': true,
+        },
+        ReturnValues: 'ALL_NEW',
+      };
+      dynamoDb.update(params3);
+    });
+  }
+
   return { status: true };
 });
